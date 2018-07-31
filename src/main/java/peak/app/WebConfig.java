8fda7ab89ac64ae4fdaa9647901c0aaec04df382
Wilson, John (TPS) <John.Wilson3@fmr.com>
2018-07-31 14:53:06 -0400

    changes to make work as a war in tomcat

--- src/main/java/peak/app/WebConfig.java
+++ src/main/java/peak/app/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        // registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 
 }
